db1b75a73d92d1df725a1f5331120cb9a6bb361c
SKARAIki <132039713+SKARAIki@users.noreply.github.com>
2025-04-17 20:20:10 +0900

    출력메세지 수정완료

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -14,6 +14,7 @@ public class Calculator {
         long inPutValue1 = scanner.nextInt();
         long numbox1 = inPutValue1;
 
+        // 추가로 사칙연산을 제외한 다른 문자를 입력하였을 때 다시 입력하세요. 라는 기능을 넣으려면?
         System.out.print("사칙연산 기호을 입력하세요: ");
         String addBox = scanner.next();
         char charBox2 = addBox.charAt(0); // 문자열 가장 앞에 있는 문자를 따오는 것
@@ -28,14 +29,6 @@ public class Calculator {
             long inPutValue3 = scanner.nextInt();
             numbox2 = inPutValue3; // 새로운 값을 위에서 선언한 변수에 재할당
         }
-
-        // char는 int, String과 달리 Scanner에서 바로 받지 못해 next()를 사용하여
-        // String을 Scanner로 받은 다음, charAt(0)로 char형으로 바꾸는 방법을 사용해야한다 - ???
-
-//        String sc = scanner.next();
-//        System.out.println("사칙연산 기호 입력: "+ sc);
-//        char box = sc.charAt(0);
-        // charAt(0)을 쓰는 방법을 찾아서 위와 같이 해보았으나 String을 쓰는게 현재로선 더 편해서 아래와 같이 사용하였습니다
 
         char add = charBox2;
         String add2 = String.valueOf(add); // 문자를 문자열로 변환
